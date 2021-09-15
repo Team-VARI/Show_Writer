eaed96935722d18139d405981710fa914c76f03e
eodn <s20063@gsm.hs.kr>
2021-09-15 21:04:54 +0900

    first commit

---
+++ Show_Writer.js
@@ -0,0 +1,14 @@
+const button = document.querySelector(".dropbtn");
+
+button.addEventListener("click", () => {
+  const dropdown = document.querySelector(".drop-content");
+  dropdown.style.display = "flex";
+});
+
+button.addEventListener("blur", () => {
+  const dropdown = document.querySelector(".drop-content");
+  // 0.2초 뒤에 실행
+  setTimeout(() => {
+    dropdown.style.display = "none";
+  }, 200);
+});
